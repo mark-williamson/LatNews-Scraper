555cf03f894cee70266647f525cf328cf38ab213
mark-williamson <mark.williamson@mail.mcgill.ca>
2018-12-06 10:35:42 -0500

    dealt with multiple title links; closer to fixing cookie box bug

--- Pre-2003_Scraper.py
+++ Pre-2003_Scraper.py
@@ -12,12 +12,15 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
-from datetime import datetime
-from selenium.webdriver.support.ui import Select
+from datetime import datetime, timedelta
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.common.by import By
 import re
+from selenium.common.exceptions import ElementNotVisibleException, WebDriverException
+from selenium.webdriver.support import expected_conditions as EC
 
-def pre2003_scrape(user, pw): 
+
+def pre2003_scrape(user, pw, search, date1, date2, loc): 
     
     # -----------------    
     # 1. Initiate browser and enter proxy credentials
@@ -45,8 +48,8 @@ def pre2003_scrape(user, pw):
     # Enter login information and click submit button
     username = driver.find_element_by_name('j_username')
     password = driver.find_element_by_name('j_password')
-    username.send_keys(my_username)
-    password.send_keys(my_password)
+    username.send_keys(user)
+    password.send_keys(pw)
     
     login_button = driver.find_element_by_name('_eventId_proceed')
     login_button.click()
@@ -58,24 +61,20 @@ def pre2003_scrape(user, pw):
     #   get cookie consent window removed
     driver.implicitly_wait(120)
     driver.find_element_by_xpath("//*[@aria-label='dismiss cookie message']").click()
-    
-    #   click button to uncheck all report boxes
-    driver.implicitly_wait(120)
-    invert_sel_button = driver.find_element_by_id("invertselectsearchlink")
-    invert_sel_button.click()
-            
+                
     #   select only WR reports -- this is kinda janky w/ the [1] but not sure how else to directly select the correct checkbox otherwise
-    driver.find_elements(By.XPATH, "//*[(@value='33')]")[1].click()
+    buttons = [str(num) for num in [3, 38, 25, 26, 28, 40, 74, 8, 7, 6, 11]]
+    driver.implicitly_wait(1000)
+    for button in range(0,len(buttons)):
+        driver.find_elements(By.XPATH, "//*[(@value='"+buttons[button]+"')]")[1].click()
 
     #   input search query (search arg is a string provided to function)
     driver.find_element_by_id("txtSearch").send_keys(search)
     
 
-    #   set date range
-    date1 = "1/1/2003" 
-    date2 = "31/1/2003"
-    date1 = datetime.strptime(date1, "%d/%m/%Y")
-    date2 = datetime.strptime(date2, "%d/%m/%Y")
+    #   set date range - change this code if input is str not datetime
+    # date1 = datetime.strptime(date1, "%d/%m/%Y")
+    # date2 = datetime.strptime(date2, "%d/%m/%Y")
     
     #   input start dates
     oSelect = Select(driver.find_element_by_id("cmbFromDay"))
@@ -104,24 +103,21 @@ def pre2003_scrape(user, pw):
     # 3. Extract article content 
     # -----------------
 
-# Return to problem here
-    driver.implicitly_wait(30)
-
     # Selenium hands the page source to Beautiful Soup
+    driver.implicitly_wait(30)
     search_results_page = BeautifulSoup(driver.page_source, 'lxml')
+    
+    # identify all links in relevant section of the page (i.e. only links to stories)
+    story_links = []
+    for link in search_results_page.findAll("a", {"class":["archive_item searchlist_a3", "archive_item searchlist_a4"]}):
+        if 'href' in link.attrs:
+            link = link.attrs['href']
+            story_links.append(link)
     
     # provide container vectors
     titles = []
     bodies = []
-    report_n = []
-    
-    # identify all links in relevant section of the page (i.e. only links to stories)
-    story_links = []
-    for link in search_results_page.findAll("a", {"class":"archive_item searchlist_a3"}):
-        if 'href' in link.attrs:
-            link = link.attrs['href']
-            story_links.append(link)
-            
+    report_n = []           
 
     # looping, travel to each link and pull relevant info
     for link in story_links:
@@ -132,12 +128,17 @@ def pre2003_scrape(user, pw):
         date = story_page.find_all("h2", string=re.compile("Weekly Report"))
         for h2 in date:
                 report_n.append(h2.text)
-                        
+                   
         # pull titles
-        title = story_page.findAll("h3")
+        title = story_page.find_all("h3")
+        title_text = []
         for title in title:
-                titles.append(title.text)
-                
+            title_text.append(title.text)
+        
+        # this join addresses cases of 'multiple' titles
+        title_text = " - ".join(title_text)
+        titles.append(title_text)
+            
         # pull bodies
         for para in story_page.find_all("div", {"class":"col-lg-12 main-content-area"}):
             
@@ -161,9 +162,6 @@ def pre2003_scrape(user, pw):
             all_text = all_text.split(title[0])[-1]
             
             bodies.append(all_text)
-
-
-# next step: write to csv
                 
     # -----------------
     # 3. Combine output into data.frame
@@ -179,13 +177,76 @@ def pre2003_scrape(user, pw):
         
     # write to .csv
     os.chdir(loc)
-    df.to_csv('articles_' + str(year) + '.csv')
+    df.to_csv('articles_' + str(date1)[:10] + '_' + str(date2)[:10] + '.csv')
 
-
+    driver.close()
 
 # actual search
 my_username = ""
 my_password = ""
 search = "Argentina OR Bahamas OR Barbados OR Belize OR Bolivia OR Brazil OR Chile OR Colombia OR Costa Rica OR Cuba OR Dominican Republic OR Ecuador OR El Salvador OR Guatemala OR Guyana OR Haiti OR Honduras OR Jamaica OR Mexico OR Nicaragua OR Panama OR Paraguay OR Peru OR Suriname OR Trinidad & Tobago OR Uruguay OR Venezuela"
 loc = '/Users/markwilliamson/Documents/RA Work/Corruption/Web scraping/Pre-2003'
+
+
+# Create start and end date lists to iterate over 
+
+def return_dates(year):
+    
+    # define first day as January 1
+    date1 = datetime.strptime("1/1/" + str(year), "%d/%m/%Y")
+
+    # Add 14 days repeatedly from Jan 1
+    start_dates = pd.date_range(start = date1, periods = 26, freq = '14D')
+    
+    # define end of each period as 13 days after start period
+    end_dates = list(start_dates + pd.DateOffset(days=13))
+    
+    # replace whatever last end period is with last day of year
+    end_dates[-1] = pd.Timestamp(year = year, month = 12, day = 31)
+
+    # convert start dates to list
+    start_dates = list(start_dates)
+    
+    return(start_dates, end_dates)
+
+start_dates, end_dates = return_dates(2003)
+
+
+# Loop over dates and run scraper on each 
+for date in range(4,len(start_dates)+1): 
+    pre2003_scrape(my_username, my_password, search, start_dates[date], end_dates[date], loc)
+
+
+
+
+
+
+''' experimenting with wait solution to cookie box
+    #   get cookie consent window removed
+    driver.implicitly_wait(1000)
+    try:
+        driver.find_element_by_xpath("//*[@aria-label='dismiss cookie message']").click()
+    except WebDriverException:
+        driver.implicitly_wait(1000)
+        driver.find_element_by_xpath("//*[@aria-label='dismiss cookie message']").click()
+    
+    #   click button to uncheck all report boxes
+    driver.implicitly_wait(1000)
+    try:
+        invert_sel_button = driver.find_element_by_id("invertselectsearchlink")
+        invert_sel_button.click()
+    except WebDriverException: 
+        driver.implicitly_wait(1000)
+        driver.find_element_by_xpath("//*[@aria-label='dismiss cookie message']").click()
+        invert_sel_button = driver.find_element_by_id("invertselectsearchlink")
+        invert_sel_button.click()
+    except ElementNotVisibleException: 
+        driver.implicitly_wait(1000)
+        driver.find_element_by_xpath("//*[@aria-label='dismiss cookie message']").click()
+        invert_sel_button = driver.find_element_by_id("invertselectsearchlink")
+        invert_sel_button.click() 
+'''
+
+
+
 
